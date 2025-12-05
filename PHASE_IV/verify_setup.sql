c5bd880f8560224a93d55d6fd6cd29164ace7a06
Uwaberagiramata <141720280+Uwaberagiramata@users.noreply.github.com>
2025-12-05 14:11:32 +0200

    Add verification SQL commands for setup
    Added SQL commands to verify setup in PHASE IV.

--- PHASE_IV/verify_setup.sql
+++ PHASE_IV/verify_setup.sql
@@ -1,1 +1,20 @@
+-- ============================================
+-- PHASE IV - VERIFY SETUP
+-- GROUP B
+-- ============================================
+
+ALTER SESSION SET CONTAINER = B_12345_Sheilla_ESDS_PDB;
+
+SELECT name, open_mode FROM v$pdbs;
+
+SHOW CON_NAME;
+
+SELECT table_name FROM user_tables;
+
+SELECT * FROM Suppliers;
+SELECT * FROM Products;
+SELECT * FROM Inventory;
+SELECT * FROM SpoilageAlerts;
+SELECT * FROM Sales;
+SELECT * FROM StorageConditions;
 
