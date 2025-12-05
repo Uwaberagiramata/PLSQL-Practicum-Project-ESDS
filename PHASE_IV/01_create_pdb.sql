e4f6f8dc6613c6e8c7065f0aacbbb3d3710340ff
Uwaberagiramata <141720280+Uwaberagiramata@users.noreply.github.com>
2025-12-05 13:51:51 +0200

    Update 01_create_pdb.sql

--- PHASE_IV/01_create_pdb.sql
+++ PHASE_IV/01_create_pdb.sql
@@ -1,1 +1,22 @@
 01_create_pdb.sql
+
+-- ============================================
+-- 01 - CREATE PDB  
+-- GROUP B – ESDS PROJECT
+-- ============================================
+
+ALTER SESSION SET container = CDB$ROOT;
+
+CREATE PLUGGABLE DATABASE B_28741_ESDS_PDB
+  ADMIN USER pdb_admin IDENTIFIED BY Admin123
+  FILE_NAME_CONVERT = (
+    '/opt/oracle/oradata/ORCLCDB/pdbseed/',
+    '/opt/oracle/oradata/ORCLCDB/B_28741_ESDS_PDB/'
+  );
+
+-- Open the PDB
+ALTER PLUGGABLE DATABASE B_28741_ESDS_PDB OPEN;
+
+-- Save state after restart
+ALTER PLUGGABLE DATABASE B_28741_ESDS_PDB SAVE STATE;
+
